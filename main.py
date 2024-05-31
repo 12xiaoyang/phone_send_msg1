de2f644571c0cc62cbc08709504ab0bbdeffeeb4
xiaolong <xiaolong@qq.com>
2024-05-31 23:05:39 +0800

    .。。

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
-"""
-主程序用来发送电脑发送图片
-"""
-import time
-
+import logging
 from Utils import Utils
 from Configuration import ConfigManager
 from EmailSender import EmailSender
-import logging
 
 
 def main():
-    # 清除logging的hander
+    # 清除logging的handler
     for handler in logging.root.handlers[:]:
         logging.root.removeHandler(handler)
+
     modify_config = input("是否需要修改配置文件 如果需要请输入yes 否则任意键退出 .....\n")
-    if modify_config == "yes":
+    if modify_config.lower() == "yes":
         ConfigManager.initialize_config(ConfigManager())
+
     config = Utils.load_config_file("config.json")
     Utils.setup_logging(config)
     image_folder = config["Folder_Path"]
     emailSender = EmailSender()
+
     while True:
-        emailSender.daily_report_job(image_folder)
-        time.sleep(60)
+        send_email = input("是否发送邮件？输入yes发送,输入其他键跳过本次发送：\n")
+        if send_email.lower() == "yes":
+            emailSender.daily_report_job(image_folder)
 
 
 if __name__ == "__main__":
     main()
-
-# /storage/emulated/0/Pictures/Screenshots/
